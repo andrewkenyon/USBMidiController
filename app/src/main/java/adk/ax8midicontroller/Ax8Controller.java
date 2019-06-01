5ea6082d6cf30da9c4559adf284b2e38a1ed52b8
Andrew Kenyon <adkenyon@gmail.com>
2019-06-01 20:59:41 +0200

    Update Ax8Controller.java

--- app/src/main/java/adk/ax8midicontroller/Ax8Controller.java
+++ app/src/main/java/adk/ax8midicontroller/Ax8Controller.java
@@ -8,6 +8,9 @@ public class Ax8Controller {
 
     byte myChannel = 0;
 
+    static final byte[] MANUFACTURER_ID = {0x00, 0x01, 0x74};
+    static final byte MODEL_ID = 0x08;
+
     public Ax8Controller() {
         this.myConnection = MidiConnection.getInstance();
     }
