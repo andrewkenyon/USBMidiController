189449f9a5f0ddb839020b28310a4f2daa529b3e
Andrew Kenyon <adkenyon@gmail.com>
2019-06-01 19:58:15 +0200

    Update Ax8Driver.java

--- app/src/main/java/adk/ax8midicontroller/Ax8Driver.java
+++ app/src/main/java/adk/ax8midicontroller/Ax8Driver.java
@@ -1,4 +1,15 @@
 class Ax8Driver {
+    private ourInstance = new Ax8Driver ();
+
+    private ourMidiDriver;
+
     private Ax8Driver () {
+    }
+
+    public Ax8Driver getInstance() {
+        return ourInstance;
+    }
+
+    public void init(Context context) {
     }
 }
