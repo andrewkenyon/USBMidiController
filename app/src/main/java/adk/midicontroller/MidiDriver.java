8441e462aef49edba002755c7d75cf39e7e300d1
Andrew Kenyon <adkenyon@gmail.com>
2019-06-03 20:46:56 +0200

    Update MidiDriver.java

--- app/src/main/java/adk/midicontroller/MidiDriver.java
+++ app/src/main/java/adk/midicontroller/MidiDriver.java
@@ -69,7 +69,8 @@ public class MidiDriver {
     private void connectToDevice(MidiDeviceInfo device, final Runnable runnable, final Handler handler) {
         myManager.openDevice(device, new MidiManager.OnDeviceOpenedListener() {
             public void onDeviceOpened(MidiDevice device) {
-                myController = new FractalController(device);
+                MidiController midiController = new MidiController(device);
+                myController = new FractalController(midiController);
 
                 handler.post(runnable);
             }
@@ -91,50 +92,5 @@ public class MidiDriver {
         } else {
             return "No devices detected";
         }
-    }
-
-    public boolean sendProgramChange(byte channel, byte program) {
-        try {
-            this.myOutput.send(new byte[]{(byte) (0xC0 + channel), program}, 0, 2);
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    public boolean sendControlChange(byte channel, byte control, byte value) {
-        try {
-            this.myOutput.send(new byte[]{(byte) (0xB0 + channel), control, value}, 0, 3);
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    public boolean sendSysExMessage(byte[] manufacturer, byte model, byte[] data) {
-        byte[] msg = new byte[data.length + manufacturer.length +3]
-        msg[0] = 0xF0;
-        for (int i = 0; i < manufacturer.length; i++) {
-            msg[i + 1] = manufacturer[i];
-        }
-        msg[manufacturer.length + 1] = model;
-        for (int i = 0; i < data.length; i++) {
-            msg[i + manufacturer.length + 2] = data[i];
-        }
-        msg[msg.length - 1] = 0xF7;
-        try {
-            this.myOutput.send(msg, 0, msg.length);
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-   public boolean sendBankChangeMsb(byte channel, byte bank) {
-         return sendControlChange(channel, CC_BANK_MSB, bank);
-    }
-
-    public boolean sendBankChangeLsb(byte channel, byte bank) {
-         return sendControlChange(channel, CC_BANK_LSB, bank);
     }
 }
